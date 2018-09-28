68d0882c267b3c0e1beb15944b794f2c8f23e5a3
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2018-09-27 21:27:08 -0500

    Funcionalidad terminada sin validaciones y sin editar

--- app/src/main/java/com/manillas/personasmaterial/AgregarPersona.java
+++ app/src/main/java/com/manillas/personasmaterial/AgregarPersona.java
@@ -1,13 +1,76 @@
 package com.manillas.personasmaterial;
 
+
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class AgregarPersona extends AppCompatActivity {
+    private EditText txtCedula, txtNombre, txtApellido;
+    private Spinner cmbSexo;
+    private String opc[];
+    private ArrayList<Integer> fotos;
+    private ImageView foto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_persona);
+        txtCedula = findViewById(R.id.txtCedula);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtApellido = findViewById(R.id.txtApellido);
+        cmbSexo = findViewById(R.id.cmbSexo);
+        foto = findViewById(R.id.foto);
+        fotos = new ArrayList<>();
+        fotos.add(R.drawable.images);
+        fotos.add(R.drawable.images2);
+        fotos.add(R.drawable.images3);
+        opc = getResources().getStringArray(R.array.sexo);
+
+    }
+
+    public int fotoAleatoria(){
+        int fotoSel;
+        Random r = new Random();
+        fotoSel = r.nextInt(this.fotos.size());
+        return fotos.get(fotoSel);
+    }
+
+    public void guardar(View v){
+        String ced, nom, ape;
+        int foto, sexo;
+        foto = this.fotoAleatoria();
+        ced = txtCedula.getText().toString();
+        nom = txtNombre.getText().toString();
+        ape = txtApellido.getText().toString();
+        sexo = cmbSexo.getSelectedItemPosition();
+        Persona p = new Persona(foto,ced,nom,ape, sexo);
+        p.guardar();
+        Snackbar.make(v, getResources().getString(R.string.guardado_exito), Snackbar.LENGTH_SHORT).show();
+        limpiar();
+    }
+
+    public void onBackPressed(){
+        finish();
+        Intent i = new Intent(AgregarPersona.this, Principal.class);
+        startActivity(i);
+    }
+    public void limpiar(){
+        txtCedula.setText("");
+        txtNombre.setText("");
+        txtCedula.setText("");
+        txtCedula.requestFocus();
+    }
+
+    public void limpiar(View v){
+        limpiar();
     }
 }
