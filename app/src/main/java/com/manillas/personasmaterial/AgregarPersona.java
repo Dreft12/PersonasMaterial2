89275a563028fe663de10e8a2cdd8039417c3121
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2018-09-27 21:30:23 -0500

    Quitando teclado luego de guardar

--- app/src/main/java/com/manillas/personasmaterial/AgregarPersona.java
+++ app/src/main/java/com/manillas/personasmaterial/AgregarPersona.java
@@ -1,11 +1,14 @@
 package com.manillas.personasmaterial;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -54,8 +57,9 @@ public class AgregarPersona extends AppCompatActivity {
         sexo = cmbSexo.getSelectedItemPosition();
         Persona p = new Persona(foto,ced,nom,ape, sexo);
         p.guardar();
-        Snackbar.make(v, getResources().getString(R.string.guardado_exito), Snackbar.LENGTH_SHORT).show();
         limpiar();
+        Snackbar.make(v, getResources().getString(R.string.guardado_exito), Snackbar.LENGTH_SHORT).show();
+
     }
 
     public void onBackPressed(){
@@ -68,6 +72,8 @@ public class AgregarPersona extends AppCompatActivity {
         txtNombre.setText("");
         txtCedula.setText("");
         txtCedula.requestFocus();
+        InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
     }
 
     public void limpiar(View v){
