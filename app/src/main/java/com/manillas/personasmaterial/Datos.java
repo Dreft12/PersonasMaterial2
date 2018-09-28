06af67a8a289bd968d120aac8dfa2bf1ff9676b0
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2018-09-27 20:23:55 -0500

    Agregando adaptador persona

---
+++ app/src/main/java/com/manillas/personasmaterial/Datos.java
@@ -0,0 +1,15 @@
+package com.manillas.personasmaterial;
+
+import java.util.ArrayList;
+
+public class Datos {
+    public static ArrayList<Persona> personas = new ArrayList<>();
+
+    public static void agregar(Persona p){
+        personas.add(p);
+    }
+
+    public static ArrayList<Persona> mostrar(){
+        return personas;
+    }
+}
